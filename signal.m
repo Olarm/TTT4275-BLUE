f35b4090820bd034910af1bef5b1277d37ee4f78
Ola Rasmus Moen <ola.moen@wellstarter.com>
2020-04-19 15:09:45 +0200

    oppdatering

--- signal.m
+++ signal.m
@@ -11,20 +11,23 @@ size(n)
 
 snr_db = -10;
 s = sigma(snr_db);
+%s = A^2 / 2 ./ db2mag(snr_db);
 mean = 0;
 
 v = s * randn(N, 1).' + mean;
 snr = 1 / (2 * s^2);
+snr_log = 10 * log10(snr);
+snr_mag = db2mag(snr_log);
 
-log_x = 1i * w_0 * n * T + phi + v;
+log_x = 1i * ( w_0 * n * T + phi + v );
 x_p = w_0 * n * T + phi + v;
-x = A * exp(log_x);
-x_u = unwrap(x_p);
+x = A * exp( log_x );
+x_u = unwrap( x_p );
 
 figure
 subplot(2,1,1)
 plot(n, x);
-title(['SNR: ',  num2str(10*log10(snr)), ' db']);
+title(['SNR: ',  num2str( snr_log ), ' db']);
 legend('x')
 subplot(2,1,2)
 plot(n, x_u)
