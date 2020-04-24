025ccf011f9ad719eb2eefdcc996554bda2e258f
Ola Rasmus Moen <ola.moen@wellstarter.com>
2020-04-24 10:50:41 +0200

    Generer e signal med forskjellige snr

--- signal.m
+++ signal.m
@@ -7,13 +7,14 @@ A = 1;
 N = 513;
 n_0 = -256;
 n = -256:1:N-257;
+n = n';
 
-H = [T * n', ones(length(n), 1)];
+H = [T * n, ones(length(n), 1)];
 C = eye(N);
 P = N * ( N - 1) / 2;
 Q = N * ( N - 1) * (2*N - 1) / 6;
 
-snr_db = 30;
+snr_db = [-10, 0, 10, 20, 30, 40];
 s = sigma(snr_db);
 %s = A^2 / 2 ./ db2mag(snr_db);
 mean = 0;
@@ -30,41 +31,49 @@ log_x = zeros(N, w);
 x_p = zeros(N, w);
 x_u = zeros(N, w);
 C_hat = zeros(N, w);
-theta_hat = zeros(N, w);
+theta_hat = zeros(2, w);
 
 
 for i=1:length(snr_db)
-    var_w_hat = 12*s(i)^2 / (A^2 * T^2 * N * ( N^2 - 1));
-    var_phi_hat = 12*s(i)^2 * (n_0^2 * N + 2*n_0 * P + Q) / (A^2 * N^2 * ( N^2 - 1));
-    v(i, :) = s(i) * randn(N, 1).' + mean;
+    CRLB_w_hat = 12*s(i)^2 / (A^2 * T^2 * N * ( N^2 - 1));
+    CRLB_phi_hat = 12*s(i)^2 * (n_0^2 * N + 2*n_0 * P + Q) / (A^2 * N^2 * ( N^2 - 1));
+    v(:, i) = s(i) * randn(N, 1).' + mean;
     snr(i) = 1 / (2 * s(i)^2);
     snr_log(i) = 10 * log10(snr(i));
     snr_mag(i) = db2mag(snr_log(i));
     
-    log_x(i, :) = 1i * ( w_0 * n * T + phi + v(i, :));
-    x_p(i, :) = w_0 * n * T + phi + v(i, :);
-    x(i, :) = A * exp( log_x(i, :));
-    x_u(i, :) = unwrap( x_p(i, :));
-
-    C_hat(i, :) = s(i)^2 * (H' / (A * A' ) * H);
-    theta_hat(i, :) = (H' / ( A * A' ) * H ) * H' / ( A * A' ) .* x(:, i);
+    log_x(:, i) = 1i * ( w_0 * n * T + phi + v(:, i));
+    x(:, i) = A * exp( log_x(:, i));
+    x_u(:, i) = unwrap(angle(x(:, i)));
+    
+    %C_hat(:, i) = s(i)^2 * (H' / (A * A' ) * H);
+    theta_hat(:, i) = (H' / ( A * A' ) * H ) * H' / ( A * A' ) * x(:, i);
 end
 
+%%%% Oppg a
 
+% figure
+% plot(n, x_u)
+% legend('-10 db', '0 db', '10 db', '20 db', '30 db', '40 db')
+% hold on
 
+% figure
+% subplot(4,1,1)
+% plot(n, x);
+% title(['SNR: ',  num2str( snr_log ), ' db']);
+% legend('x')
+% subplot(4,1,2)
+% plot(n, unwrap(angle(x)))
+% legend('unwrap(x)')
+% subplot(4,1,3)
+% plot(n, var(theta_hat(1,:)))
+% title(['var: ',  num2str( var(theta_hat(1,:)))]);
+% legend('omega^')
+% subplot(4,1,4)
+% plot(n, var(theta_hat(2,:)))
+% title(['var: ',  num2str( var(theta_hat(2,:)))]);
+% legend('phi^')
+
+%%%% Oppg b
 figure
-subplot(4,1,1)
-plot(n, x);
-title(['SNR: ',  num2str( snr_log ), ' db']);
-legend('x')
-subplot(4,1,2)
-plot(n, unwrap(angle(x)))
-legend('unwrap(x)')
-subplot(4,1,3)
-plot(n, var(theta_hat(1,:)))
-title(['var: ',  num2str( var(theta_hat(1,:)))]);
-legend('omega^')
-subplot(4,1,4)
-plot(n, var(theta_hat(2,:)))
-title(['var: ',  num2str( var(theta_hat(2,:)))]);
-legend('phi^')
+plot(n, CRLB_w_hat*ones(length(n)), n, theta_hat)
